96fabc74c06292cc67d8b325213e1b13a719f09d
Muhammadsoleh <msoleh07@gmail.com>
2023-11-11 22:36:20 +0500

    addet header none

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <Header />
+          {/* <Header /> */}
           {children}
         </div>
       </body>
